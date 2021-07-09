4e0711e273690cf221546049e7f4eee68b35d500
Wayne DIxon <dev@waynedixon.com>
2021-07-09 07:46:55 -0500

    Initial Commit

---
+++ Shared/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  Shared
+//
+//  Created by Wayne Dixon on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
